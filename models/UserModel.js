e45029071c847ba1fa3e7ed424faacb38e6fcb99
Onuora Ebube Ebuka <onuoraebube44@gmail.com>
2024-09-13 09:42:53 +0100

    fixed little bugs

--- models/UserModel.js
+++ models/UserModel.js
@@ -2,24 +2,8 @@
 
 
 import mongoose, { Schema } from 'mongoose';
-import bcrypt from 'bcrypt';
 
-// Profile image name and collection lists
-// const profileImgsNameList = [
-//     'Garfield', 'Tinkerbell', 'Annie', 'Loki', 'Cleo', 'Angel', 'Bob', 
-//     'Mia', 'Coco', 'Gracie', 'Bear', 'Bella', 'Abby', 'Harley', 
-//     'Cali', 'Leo', 'Luna', 'Jack', 'Felix', 'Kiki'
-// ];
-// const profileImgsCollectionsList = [
-//     'notionists-neutral', 'adventurer-neutral', 'fun-emoji'
-// ];
 
-// // Generate default profile image URL
-// const generateProfileImageUrl = () => {
-//     const name = profileImgsNameList[Math.floor(Math.random() * profileImgsNameList.length)];
-//     const collection = profileImgsCollectionsList[Math.floor(Math.random() * profileImgsCollectionsList.length)];
-//     return `https://api.dicebear.com/6.x/${collection}/svg?seed=${name}`;
-// };
 let profile_imgs_name_list = ["Garfield", "Tinkerbell", "Annie", "Loki", "Cleo", "Angel", "Bob", "Mia", "Coco", "Gracie", "Bear", "Bella", "Abby", "Harley", "Cali", "Leo", "Luna", "Jack", "Felix", "Kiki"];
 let profile_imgs_collections_list = ["notionists-neutral", "adventurer-neutral", "fun-emoji"];
 
@@ -44,18 +28,4 @@ const userSchema = new mongoose.Schema({
     timestamps: { createdAt: 'joinedAt' } // Ensure correct placement of timestamps option
 });
 
-// Middleware to hash the password before saving
-// userSchema.pre('save', async function (next) {
-//     if (!this.isModified('password')) return next();
-
-//     try {
-//         const salt = await bcrypt.genSalt(10);
-//         this.password = await bcrypt.hash(this.password, salt);
-//         next();
-//     } catch (error) {
-//         next(error);
-//     }
-// });
-
-// Export the User model
 export default mongoose.model('User', userSchema);
